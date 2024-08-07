bd0431e204d8fe501ba412f116ded0eb1e78936d
ofv-projects <o_firmwarevault@proton.me>
2024-08-07 18:46:45 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,24 +3,28 @@ window.addEventListener('DOMContentLoaded', () => {
   const resultsContainer = document.getElementById('results');
   const searchButton = document.getElementById('searchButton');
   
-  searchButton.addEventListener('click', () => {
-
+  searchButton.addEventListener('click', async () => {
   const partNumber = partNumberInput.value;
-  fetch(`https://ofv-searchup.onrender.com/versions?partNumber=${partNumber}`)
+
+  fetch(`https://ofv-searchup.onrender.com/api/versions?partNumber=${partNumber}`) 
     .then(response => response.json())
     .then(versions => {
-      resultsContainer.innerHTML = '';
+      resultsContainer.innerHTML = ''; 
       if (versions.length > 0) {
         versions.forEach(version => {
           const resultItem = document.createElement('div');
           resultItem.innerHTML = `
-            <h3>Version ${version.versionNumber}</h3>
-            <a href="https://ofv-searchup.onrender.com/public/uploads/${version.filename}" download>Download</a>
+            <h3>Version ${version.version_number}</h3> 
+            <a href="${version.download_link}" target="_blank">Download</a> 
           `;
           resultsContainer.appendChild(resultItem);
         });
       } else {
         resultsContainer.innerHTML = '<p>Keine Versionen gefunden</p>';
       }
+    })
+    .catch(error => {
+      console.error('Fehler bei der Anfrage:', error);
+      resultsContainer.innerHTML = '<p>Fehler beim Laden der Versionen</p>';
     });
-  })});
+})})
